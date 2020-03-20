2801502b00d0cd9dfea0f7c9b5ebf382a71208da
Carson <zmcddn@hotmail.com>
2020-03-20 10:00:41 -0600

    Add tests for place function

--- tests/test_tic_tac_toe.py
+++ tests/test_tic_tac_toe.py
@@ -1,3 +1,8 @@
+from unittest.mock import patch
+import io
+
+import pytest
+
 from tic_tac_toe import TicTacToeGame
 
 
@@ -7,3 +12,18 @@ def test_board_empty():
     for key in game.board.keys():
         assert game.board[key] == "-"
 
+
+def test_place_already_taken():
+    game = TicTacToeGame()
+    game.place(3)
+    game.place(3)
+
+    with patch('sys.stdout', new=io.StringIO()) as stdout:
+        game.place(3)
+    assert stdout.getvalue() == "Position already taken, please try again:\n"
+
+
+def test_place_valid():
+    game = TicTacToeGame()
+    game.place(3)
+    assert game.board[3] == "X"
